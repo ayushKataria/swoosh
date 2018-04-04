092d1c53484b5f7d1abeedcd772940d86fa9b707
ayushKataria <akat21121997Agmail.com>
2018-04-04 23:28:01 +0530

    lesson 07

---
+++ app/src/main/java/com/example/akat2/swoosh/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.akat2.swoosh
+
+/**
+ * Created by Ayush Kataria on 04-04-2018.
+ */
+
+const val EXTRA_LEAGUE = "league"
--- app/src/main/java/com/example/akat2/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/akat2/swoosh/LeagueActivity.kt
@@ -3,8 +3,12 @@ package com.example.akat2.swoosh
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,7 +16,31 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun leagueNextClicked(view: View) {
-        val skillIntent = Intent(this, SkillActivity::class.java)
-        startActivity(skillIntent)
+        if(menLeagueBtn.isChecked || womenLeagueBtn.isChecked || coedLeagueBtn.isChecked) {
+            val skillIntent = Intent(this, SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE, selectedLeague)
+            startActivity(skillIntent)
+        }else {
+            Toast.makeText(this, "Please select a league", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun onMenClicked(view: View) {
+        womenLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        selectedLeague = "men"
+    }
+
+    fun onWomenClicked(view: View) {
+        menLeagueBtn.isChecked = false
+        coedLeagueBtn.isChecked = false
+        selectedLeague = "women"
+
+    }
+
+    fun onCoedClicked(view: View) {
+        menLeagueBtn.isChecked = false
+        womenLeagueBtn.isChecked = false
+        selectedLeague = "coed"
     }
 }
