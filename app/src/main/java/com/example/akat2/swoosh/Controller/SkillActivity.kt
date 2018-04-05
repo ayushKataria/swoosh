78fcd2beceadb4ef8923214d05fec3c6dbe8e029
ayushKataria <akat21121997Agmail.com>
2018-04-05 18:45:56 +0530

    lesson 10

--- app/src/main/java/com/example/akat2/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/akat2/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.example.akat2.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.akat2.swoosh.Model.Player
@@ -14,11 +15,23 @@ class SkillActivity : BaseActivity() {
     //Initialize is later
     lateinit var player: Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
 
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onBallerClicked(view: View) {
